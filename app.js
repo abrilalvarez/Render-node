a96f9cc8d9f85f565ae2d1dc6d7161183bc9ea38
abrilalvarez <amoedomonica@yahoo.com.ar>
2022-10-25 22:58:08 -0300

    commit 1 mercado liebre

---
+++ app.js
@@ -0,0 +1,50 @@
+const express = require('express');
+
+const path = require('path');
+
+const app = express();
+
+app.use(express.static('public'));
+
+// app.use("/static", express.static(__dirname + "/public"));
+
+app.listen(process.env.PORT || 3000, () => console.log('Servidor activo.'));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/home.html"))
+}
+)
+
+app.get("/ofertas", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/ofertas.html"))
+}
+)
+
+app.get("/tiendasoficiales", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/tiendasOficiales.html"))
+}
+)
+app.get("/vender", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/vender.html"))
+}
+)
+app.get("/ayuda", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/ayuda.html"))
+}
+)
+app.get("/register", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/register.html"))
+}
+)
+app.get("/login", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/login.html"))
+}
+)
+
+app.post("/register", (req,res) => {
+    res.redirect("/");
+})
+
+app.post("/login", (req,res) => {
+    res.redirect("/");
+})
